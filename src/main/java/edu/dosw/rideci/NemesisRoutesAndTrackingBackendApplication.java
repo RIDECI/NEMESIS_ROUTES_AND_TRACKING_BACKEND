08679ad0a3bfe0c2f754f6f09a358b08ca3f5194
Tulio <tuliojr3101@gmail.com>
2025-11-08 20:23:33 -0500

    feat(core): Initial Project Structure

---
+++ src/main/java/edu/dosw/rideci/NemesisRoutesAndTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.NEMESIS_ROUTES_AND_TRACKING_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemesisRoutesAndTrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemesisRoutesAndTrackingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/rideci/NEMESIS_ROUTES_AND_TRACKING_BACKEND/NemesisRoutesAndTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.NEMESIS_ROUTES_AND_TRACKING_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemesisRoutesAndTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
