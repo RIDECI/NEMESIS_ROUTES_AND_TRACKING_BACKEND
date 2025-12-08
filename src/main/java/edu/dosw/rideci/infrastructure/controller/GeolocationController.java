4bdc8fec3dfbda6d2e7835e1e61d534f90762d76
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2025-12-08 17:16:30 -0500

    add(core): Agregando endpoint para actualizar ubicacion

--- src/main/java/edu/dosw/rideci/infrastructure/controller/GeolocationController.java
+++ src/main/java/edu/dosw/rideci/infrastructure/controller/GeolocationController.java
@@ -2,13 +2,17 @@ package edu.dosw.rideci.infrastructure.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.dosw.rideci.application.mapper.RouteMapperInitial;
 import edu.dosw.rideci.application.port.in.GetRouteInformationUseCase;
+import edu.dosw.rideci.application.port.in.UpdateLocationUseCase;
+import edu.dosw.rideci.domain.model.Location;
 import edu.dosw.rideci.domain.model.Route;
+import edu.dosw.rideci.infrastructure.controller.dto.response.LocationResponse;
 import edu.dosw.rideci.infrastructure.controller.dto.response.RouteResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class GeolocationController {
 
     private final GetRouteInformationUseCase getRouteInformationUseCase;
+    private final UpdateLocationUseCase updateLocationUseCase;
     private final RouteMapperInitial routeMapperInitial;
 
     @GetMapping("/{travelId}")
@@ -26,6 +31,16 @@ public class GeolocationController {
         Route routeInformation = getRouteInformationUseCase.getRouteInformation(travelId);
 
         return ResponseEntity.ok(routeMapperInitial.toResponse(routeInformation));
+    }
+
+    @PatchMapping
+    public ResponseEntity<LocationResponse> updateLocation(
+            @PathVariable String travelId,
+            @PathVariable Location newLocation){
+
+        Location location = updateLocationUseCase.updateLocation(travelId, newLocation);
+
+        return ResponseEntity.ok(routeMapperInitial.toLocationResponse(location)); 
     }
 
 }
